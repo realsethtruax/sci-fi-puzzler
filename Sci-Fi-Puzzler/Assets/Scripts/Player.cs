8c0ff676545f8eaedb4c11f4fcc0da99b577c843
realsethtruax <seth_truax@outlook.com>
2023-01-12 22:27:54 -0500

    updated tilemap, added to player class

---
+++ Sci-Fi-Puzzler/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    void Update()
+    {
+    }
+
+    public void Grab(InputAction.CallbackContext context) {
+        if (context.performed) { 
+            Debug.Log("Grab! " + context.phase);
+        }
+    }
+}
