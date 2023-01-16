c73de4914349d621bf9a21f4ce021b78a0442f8f
realsethtruax <seth_truax@outlook.com>
2023-01-15 21:55:01 -0500

    added switches for puzzles

---
+++ Sci-Fi-Puzzler/Assets/Scripts/SwitchSystem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchSystem : Puzzle
+{
+    [SerializeField] private Switch[] _switches;
+
+    public void UpdateSystem()
+    {
+        bool isComplete = true;
+        foreach (Switch item in _switches)
+        {
+            if (item.GetSwitchState() == Switch.SwitchState.UNPRESSED) {
+                isComplete = false;
+            }
+        }
+        if (isComplete) {
+            foreach (var swi in _switches)
+            {
+                swi.ChangeSwitchState(Switch.SwitchState.SOLVED);
+            }
+            _isSolved = true;
+        }
+    }
+}
