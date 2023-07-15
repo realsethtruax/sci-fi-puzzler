4e1e6b7923e2ac45cf3dd2b1ef8ec0cf037c681e
realsethtruax <seth_truax@outlook.com>
2023-07-15 00:43:54 -0400

    Added code comment

--- Sci-Fi-Puzzler/Assets/Scripts/Puzzle.cs
+++ Sci-Fi-Puzzler/Assets/Scripts/Puzzle.cs
@@ -6,4 +6,6 @@ public class Puzzle : MonoBehaviour
 {
     [SerializeField] protected bool _isSolved;
     [SerializeField] protected PuzzleSolution _solution;
+
+    //Comment for demo purposes - please remove me if you see me :)
 }
